d24bfe7025c0d1cdf563d9119a66a11d818c4660
Gunasekaran Namachivayam <gunasekar@live.com>
2026-06-27 00:01:33 +0530

    feat(ranger): add nvim command alias
    Keep the existing :code command and add :n as a shorter alias for opening the current directory in Neovim.

--- ranger/.config/ranger/commands.py
+++ ranger/.config/ranger/commands.py
@@ -1,12 +1,20 @@
 from ranger.api.commands import Command
 
-class code(Command):
-  """
-  :code
-  Opens current directory in Neovim
-  """
 
-  def execute(self):
-    dirname = self.fm.thisdir.path
-    open_codebase_cmd = ["nvim", dirname]
-    self.fm.execute_command(open_codebase_cmd)
+class code(Command):
+    """
+    :code
+    Opens current directory in Neovim
+    """
+
+    def execute(self):
+        dirname = self.fm.thisdir.path
+        open_codebase_cmd = ["nvim", dirname]
+        self.fm.execute_command(open_codebase_cmd)
+
+
+class n(code):
+    """
+    :n
+    Alias for :code
+    """
